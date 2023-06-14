fc99000c30a823f4ce8c421b5fdc9ae57de591b7
casperwoud <c.n.woudenberg@student.vu.nl>
2023-06-14 12:58:56 +0200

    Added start  of heuristic python script.

---
+++ heuristics/heur_function_diff.py
@@ -0,0 +1,24 @@
+from git import Repo
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='This script returns a tuple of all function names in a git repository and the corresponding weights')
+    parser.add_argument('repo_path', type=str,
+                    help='Path to git repository.')
+    return parser.parse_args()
+
+def itterate_changes(repo):
+    for commit in repo.iter_commits():
+        # The M flag in the iter_change_type method indicates that only modofications are itterated.
+        for change in commit.diff().iter_change_type("M"):
+            print(change)
+
+
+def main():
+    args = parse_args()
+    repo = Repo(args.repo_path)
+    itterate_changes(repo)
+    
+
+if __name__ == "__main__":
+    main()
