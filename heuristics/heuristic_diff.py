3457e485c911401becd43acba18b99b850737867
casperwoud <c.n.woudenberg@student.vu.nl>
2023-06-21 17:15:01 +0200

    script now finds functions with clang ast.

--- heuristics/heuristic_diff.py
+++ heuristics/heuristic_diff.py
@@ -14,26 +14,34 @@ def parse_args():
 # This function traverses a repo and returns all function names according to a pattern
 # that SHOULD represent most of the c type function declarations.
 def search_funcs(directory):
-    pattern = '^\s*(?:(?:inline|static)\s+){0,2}(?!else|typedef|return)\w+\s+\*?\s*(\w+)\s*\([^0]+\)\s*;?'
+    # This pattern finds a word that is followed up with a space and then a single quote.
+    pattern_func_name = r".*FunctionDecl.*\b(\w+)\b(?=\s*')"
+    #These are words that can't be in the ast dump.
+    forbidden_words = ['extern', 'implicit', 'invalid', '__']
 
     funcs = []
 
     for root, dirs, files in walk(directory):
         for file_str in files:
-            if file_str[-2:] == '.c' or file_str[-2] == '.h':
+            # Only find functions in .c files.
+            if file_str[-2:] == '.c':
                 file_path = join(root, file_str)
                 # Excludes testing function from function list.
                 if 'test' in file_path:
                     continue
-                file = open(file_path, 'r')
-                for line in file:
-                    match = re.search(pattern, line)
-                    if match:
-                        func_name = match.group(1)
-                        funcs.append((func_name, file_path))
+                ast_funcs = run_command('clang -Xclang -ast-dump -fsyntax-only ' + file_path + ' 2>/dev/null | grep FunctionDecl')
+                for line in ast_funcs.splitlines():
+                    found = False
+                    for word in forbidden_words:
+                        if word in line:
+                            found = True
+                        if not found:
+                            match = re.search(pattern_func_name, line)
+                            if match:
+                                func_name = match.group(1)
+                                print(func_name)
+                                funcs.append(func_name)
                         
-                
-
     return funcs
 
 # Function that starts a subprocess, mostly used to check the git logs.
@@ -48,12 +56,14 @@ def run_command(command):
 def main():
     args = parse_args()
     weighted_funcs = []
+    
     funcs = search_funcs(args.repo_path)
-    for func in funcs:
-        func_freq = run_command('git log --no-patch -L :' + func[0] + ':' + func[1] + ' | grep -c commit')
-        weighted_funcs.append((func[0], func_freq))
+    print(funcs)
+    # for func in funcs:
+    #     func_freq = run_command('cd ' + args.repo_path + '; git log --no-patch -L :' + func[0] + ':' + func[1] + ' | grep -c commit')
+    #     weighted_funcs.append((func[0], func_freq))
 
-    print(weighted_funcs)
+    # print(weighted_funcs)
 
 if __name__ == "__main__":
     main()
