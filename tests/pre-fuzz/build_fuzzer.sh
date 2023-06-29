14e929cc60702ba77a78ae118e95b3e317f37d2a
jtro0 <j.e.troost@student.vu.nl>
2023-06-29 09:28:12 +0000

    testing with own scripts

--- tests/pre-fuzz/build_fuzzer.sh
+++ tests/pre-fuzz/build_fuzzer.sh
@@ -1,2 +1,58 @@
-cd $AFL
-docker build -t aflplusplus .
+#!/bin/bash
+set -e
+
+OLD_DIR=$PWD
+# Preinstall
+apt-get update && \
+    apt-get install -y make clang-15 llvm-15-dev libc++-15-dev libc++abi-15-dev \
+        build-essential git wget gcc-12-plugin-dev
+
+update-alternatives \
+  --install /usr/lib/llvm              llvm             /usr/lib/llvm-15  20 \
+  --slave   /usr/bin/llvm-config       llvm-config      /usr/bin/llvm-config-15  \
+    --slave   /usr/bin/llvm-ar           llvm-ar          /usr/bin/llvm-ar-15 \
+    --slave   /usr/bin/llvm-as           llvm-as          /usr/bin/llvm-as-15 \
+    --slave   /usr/bin/llvm-bcanalyzer   llvm-bcanalyzer  /usr/bin/llvm-bcanalyzer-15 \
+    --slave   /usr/bin/llvm-c-test       llvm-c-test      /usr/bin/llvm-c-test-15 \
+    --slave   /usr/bin/llvm-cov          llvm-cov         /usr/bin/llvm-cov-15 \
+    --slave   /usr/bin/llvm-diff         llvm-diff        /usr/bin/llvm-diff-15 \
+    --slave   /usr/bin/llvm-dis          llvm-dis         /usr/bin/llvm-dis-15 \
+    --slave   /usr/bin/llvm-dwarfdump    llvm-dwarfdump   /usr/bin/llvm-dwarfdump-15 \
+    --slave   /usr/bin/llvm-extract      llvm-extract     /usr/bin/llvm-extract-15 \
+    --slave   /usr/bin/llvm-link         llvm-link        /usr/bin/llvm-link-15 \
+    --slave   /usr/bin/llvm-mc           llvm-mc          /usr/bin/llvm-mc-15 \
+    --slave   /usr/bin/llvm-nm           llvm-nm          /usr/bin/llvm-nm-15 \
+    --slave   /usr/bin/llvm-objdump      llvm-objdump     /usr/bin/llvm-objdump-15 \
+    --slave   /usr/bin/llvm-ranlib       llvm-ranlib      /usr/bin/llvm-ranlib-15 \
+    --slave   /usr/bin/llvm-readobj      llvm-readobj     /usr/bin/llvm-readobj-15 \
+    --slave   /usr/bin/llvm-rtdyld       llvm-rtdyld      /usr/bin/llvm-rtdyld-15 \
+    --slave   /usr/bin/llvm-size         llvm-size        /usr/bin/llvm-size-15 \
+    --slave   /usr/bin/llvm-stress       llvm-stress      /usr/bin/llvm-stress-15 \
+    --slave   /usr/bin/llvm-symbolizer   llvm-symbolizer  /usr/bin/llvm-symbolizer-15 \
+    --slave   /usr/bin/llvm-tblgen       llvm-tblgen      /usr/bin/llvm-tblgen-15
+
+update-alternatives \
+  --install /usr/bin/clang                 clang                  /usr/bin/clang-15     20 \
+  --slave   /usr/bin/clang++               clang++                /usr/bin/clang++-15 \
+  --slave   /usr/bin/clang-cpp             clang-cpp              /usr/bin/clang-cpp-15
+
+apt-get update && \
+    apt-get install -y unzip openjdk-17-jdk-headless
+
+# Build fuzzer
+if [ ! -d "$FUZZER" ]; then
+    echo "Fuzzer folder does not exist"
+    exit 1
+fi
+
+cd "$FUZZER"
+export CC=clang
+export CXX=clang++
+# export AFL_NO_X86=1
+export PYTHON_INCLUDE=/
+make -j$(nproc) || exit 1
+make -C utils/aflpp_driver || exit 1
+
+mkdir -p "$OUT/afl" "$OUT/cmplog"
+cd $OLD_DIR
+
---
+++ tests/pre-fuzz/instrument.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -e
+
+# Instrument
+export CC="$FUZZER/afl-clang-fast"
+export CXX="$FUZZER/afl-clang-fast++"
+export AS="llvm-as"
+
+export LIBS="$LIBS -lc++ -lc++abi $FUZZER/utils/aflpp_driver/libAFLDriver.a"
+
+# AFL++'s driver is compiled against libc++
+export CXXFLAGS="$CXXFLAGS -stdlib=libc++"
+
+# python3 $FUZZER//heuristics/heuristic_diff.py $TARGET/repo > $TARGET/heuristics.txt
+# cat $TARGET/heuristics.txt
+
+mkdir -p $FUZZER/headless
+
+# Build the AFL-only instrumented version
+(
+    export OUT="$OUT/afl"
+    export LDFLAGS="$LDFLAGS -L$OUT"
+
+    "$MAGMA/build.sh"
+    "$TARGET/build.sh"
+    export DATA_PATH=$TARGET/heuristics.txt
+    for PROG in $OUT/*; do
+        if [ -x $PROG ]
+        then
+            export PROG=$PROG
+            $FUZZER/ghidra/ghidra_10.3.1_PUBLIC/support/analyzeHeadless $FUZZER/headless Scripting -import $PROG -overwrite -scriptPath $FUZZER/repo_temp/ghidra_scripts -postScript get_bitmap_offsets.py 
+        else
+            echo "Not executable"
+        fi
+    done
+)
+
+# Build the CmpLog instrumented version
+
+(
+    export OUT="$OUT/cmplog"
+    export LDFLAGS="$LDFLAGS -L$OUT"
+    # export CFLAGS="$CFLAGS -DMAGMA_DISABLE_CANARIES"
+
+    export AFL_LLVM_CMPLOG=1
+
+    "$MAGMA/build.sh"
+    "$TARGET/build.sh"
+    export DATA_PATH=$TARGET/heuristics.txt
+
+    for PROG in $OUT/*; do
+        if [ -x $PROG ]
+        then
+            export PROG=$PROG
+            $FUZZER/ghidra/ghidra_10.3.1_PUBLIC/support/analyzeHeadless $FUZZER/headless Scripting -import $PROG -overwrite -scriptPath $FUZZER/repo_temp/ghidra_scripts -postScript get_bitmap_offsets.py $TARGET/heuristics.txt
+        else
+            echo "Not executable"
+        fi
+    done
+)
